23e56726bd84d9eb2e9425ed1610e8331ecd8c76
dnlclmnt <dnlclmnt@gmail.com>
2022-09-20 10:25:59 -0400

    created directories, component files, pages files, installed react router and reactstrap, created routes, and mockCats.js file

---
+++ cat-tinder/src/mockCats.js
@@ -0,0 +1,32 @@
+const mockCats = [
+    {
+        id: 1,
+        name: "Buttercup",
+        age: 2,
+        enjoys: "laser lights",
+        image: "https://excitedcats.com/wp-content/uploads/2021/11/grey-and-white-persian-kitten_TeeCanDit_Shutterstock.jpg"
+    },
+    {
+        id: 2,
+        name: "Pawl",
+        age: 3,
+        enjoys: "lounging",
+        image: "https://vetstreet.brightspotcdn.com/dims4/default/303f54d/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F29%2Fb6%2Ff4a864144d09974dfe5bf0513e20%2FPersian-AP-V8HE5B-645sm3614.jpg"
+    },
+    {
+        id: 3,
+        name: "Pawston",
+        age: 4,
+        enjoys: "hot weather",
+        image: "https://img1.wsimg.com/isteam/ip/c8eba154-f5e3-4b4f-8dfd-e50a853b5a8e/E6C13BF3-16D1-47EA-B20E-827EA8FE6FB1.jpeg"
+    },
+    {
+        id: 4,
+        name: "Hairy Styles",
+        age: 1,
+        enjoys: "destroying furniture",
+        image: "https://www.leadershiptangles.com/hs-fs/hub/80933/file-15714497-jpg/images/cat-resized-600.jpg"
+    }
+]
+
+export default mockCats
