93491a520124d7b0a83c24b49a041e16e3c67a00
dnlclmnt <dnlclmnt@gmail.com>
2022-09-20 17:39:48 -0400

    moved files outside of nested app directory

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+        <footer>
+        <p>
+            Site Made By: Cathrine and Daniel
+        </p>
+        </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NotFoundCat from '../assets/NotFound.jpeg'
+
+const NotFound = () => {
+    return (
+        <>
+        <div>Page Not Found</div>
+        <img src={NotFoundCat}>
+        </img>
+        </>
+    )
+}
+
+export default NotFound
