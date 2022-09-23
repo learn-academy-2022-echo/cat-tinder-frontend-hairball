03ffd80e02ec01cffcf44fac28b77633edd370fd
CathrineC <cjsr.carreon@gmail.com>
2022-09-23 14:59:26 -0700

    CSS update and images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import catcouple from "../assets/catcouple.png"
-import { BrowserRouter } from 'react-router-dom'
+import catcouple from "../assets/cattinder_logo.png"
+import allcats from "../assets/allcats.png"
+import addcat from "../assets/addcat.png"
+import { NavLink } from "react-router-dom"
 
 const Home = () => {
     return (
         <main>
-            <h1>Welcome to Cat Tinder</h1>
-            <img src={catcouple} alt="silhouette of a cat couple"/>
+            <h1>Welcome to</h1>
+            <div >
+                <img src={catcouple} alt="silhouette of a cat couple" className='picture'/>
+            </div>
+            <p>
+                <NavLink to="/catindex"><img src={allcats} alt="All Cats" className='home_button'/>
+                </NavLink>
+                <NavLink to="/catnew"><img src={addcat} alt="Add a Cat" className='home_button'/>
+                </NavLink>
+            </p>
         </main>
     )
 }
