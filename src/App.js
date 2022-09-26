727fc459407b9e942f9cff79dad080a018c53ff4
dnlclmnt <dnlclmnt@gmail.com>
2022-09-26 01:15:45 -0400

    completed fetch with read, create, update, and delete functionality. added delete button on catshow page. updated form to have values of cat already on the page to eliminate the need to fill all fields for validation checks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Home from './pages/Home'
 import CatIndex from './pages/CatIndex'
@@ -10,18 +10,61 @@ import CatEdit from './pages/CatEdit'
 import NotFound from './pages/NotFound'
 import Footer from './components/Footer'
 import { Routes, Route } from "react-router-dom"
-import mockCats from './mockCats'
 
 function App() {
-  const [ cats, setCats ] = useState(mockCats)
+  const [ cats, setCats ] = useState([])
+
+  useEffect(() => {
+    readCat()
+}, [])
+
+
+  const readCat = () => {
+    fetch("http://localhost:3000/cats")
+    .then((response) => response.json())
+    .then((payload) => setCats(payload))
+    .catch((error) => console.log(error))
+  }
 
   const createCat = (cat) => {
+    fetch("http://localhost:3000/cats", {
+      body: JSON.stringify(cat),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+    .then((response) => response.json())
+    .then((payload) => readCat())
+    .catch((errors) => console.log("Creating a cat errors: ", errors))
     console.log("Cat has been created", cat)
   }
 
   const updateCat = (cat, id) => {
+    fetch(`http://localhost:3000/cats/${id}`, {
+      body: JSON.stringify(cat),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "PATCH"
+    })
+    .then((response) => response.json())
+    .then((payload) => readCat())
+    .catch((errors) => console.log("Updating errors", errors))
     console.log("cat:", cat)
     console.log("id:", id)
+  }
+
+  const deleteCat = (id) => {
+    fetch(`http://localhost:3000/cats/${id}`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+    .then((payload) => readCat())
+    .catch((errors) => console.log("Deleting errors:", errors))
   }
 
   return (
@@ -30,7 +73,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/catindex" element={<CatIndex cats={cats} />} />
-      <Route path="/catshow/:id" element={<CatShow cats={cats} />} />
+      <Route path="/catshow/:id" element={<CatShow cats={cats} deleteCat={deleteCat} />} />
       <Route path="/catnew" element={<CatNew createCat={createCat}/>} />
       <Route path="/catedit/:id" element={<CatEdit cats={cats} updateCat={updateCat}/>} />
       <Route path="/*" element={<NotFound />} />
--- src/pages/CatEdit.test.js
+++ src/pages/CatEdit.test.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react'
 import CatEdit from './CatEdit'
 import { screen, render } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import mockCats from '../mockCats'
 
 describe('<CatEdit/>', () => {
     it("CatEdit page renders for the user", () => {
         render(
-            <BrowserRouter>
-                <CatEdit/>
-            </BrowserRouter>
+            <MemoryRouter initialEntries={["/catedit/1"]}>
+            <Routes>
+              <Route path='catedit/:id' element={<CatEdit cats={mockCats} />}/>
+            </Routes>
+          </MemoryRouter>
         )
-        expect(screen.getByText("Edit Cat")).toBeInTheDocument()
+        expect(screen.getByText(`Edit ${mockCats[0].name}`)).toBeInTheDocument()
     })
     it("displays a form for the user", () => {
         render(
-            <BrowserRouter>
-                <CatEdit/>
-            </BrowserRouter>
+            <MemoryRouter initialEntries={["/catedit/1"]}>
+            <Routes>
+              <Route path='catedit/:id' element={<CatEdit cats={mockCats} />}/>
+            </Routes>
+          </MemoryRouter>
         )
         const formThing = screen.getAllByRole('textbox')
         screen.debug(formThing)
